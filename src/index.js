b3dd6ef3eb859e0eed2784328c34fc4c701a8c4b
YanaHrebneva <ygrebneva96@gmail.com>
2021-08-30 21:35:22 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-function eval() {
-    // Do not use eval!!!
-    return;
-}
-
 function expressionCalculator(expr) {
-    // write your solution here
+    if (expr.includes('/ 0') || expr.includes("/0")) {
+        throw new Error('TypeError: Division by zero.');
+    } else if((expr.match(/\(/g)||[]).length === (expr.match(/\)/g)||[]).length ){
+        return Function(`'use strict'; return (${expr})`)()
+    } else {
+        throw new Error('ExpressionError: Brackets must be paired')
+    }
 }
 
 module.exports = {
